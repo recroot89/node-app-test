0bd2f268cbdd63a088da48f948e27da07d2c787f
recroot89 <recroot89@gmail.com>
2020-11-30 16:00:22 +0500

    init commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const path = require('path')
+const exphbs = require('express-handlebars')
+
+const app = express()
+
+const hbs = exphbs.create({
+  defaultLayout: 'main',
+  extname: 'hbs'
+})
+
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.set('views', 'views')
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.render('index')
+  // res.sendFile(path.join(__dirname, 'views', 'index.html'))
+})
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`)
+})
