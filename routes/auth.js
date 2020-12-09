dbac42f9272bad89a4a174fc87b742daa5ee0ce2
recroot89 <recroot89@gmail.com>
2020-12-09 17:05:55 +0500

    add user login/logout and register routes

--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,6 @@
 const { Router } = require('express')
+const User = require('../models/user')
+
 const router = Router()
 
 router.get('/login', async (req, res) => {
@@ -8,4 +10,40 @@ router.get('/login', async (req, res) => {
   })
 })
 
+router.post('/login', async (req, res) => {
+  const { email } = req.body
+  const user = await User.findOne({ email })
+  req.session.user = user
+  req.session.isAuthenticated = true
+  req.session.save((err) => {
+    if (err) {
+      throw err
+    }
+    res.redirect('/')
+  })
+})
+
+router.get('/logout', async (req, res) => {
+  req.session.destroy(() => {
+    res.redirect('/')
+  })
+})
+
+router.post('/register', async (req, res) => {
+  try {
+    const { email, password, repeat, name } = req.body
+    const existingUser = await User.findOne({ email })
+    if (existingUser) {
+      res.redirect('/auth/login#register')
+    } else {
+      const user = new User({
+        email, password, name, cart: { items: [] }
+      })
+      await user.save()
+      res.redirect('/auth/login#login')
+    }
+  } catch (err) {
+    console.log(err)
+  }
+})
 module.exports = router
